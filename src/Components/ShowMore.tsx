5b7a56741d7c4d8e1509c00fe3f50237315bce3b
shahjalal <lx.shahjalal@gmail.com>
2023-09-12 22:32:24 +0600

    completed

---
+++ src/Components/ShowMore.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { updateSearchParams } from '@/utils';
+import { useRouter } from 'next/navigation';
+import Button from './CustomButton';
+import { ShowMoreProps } from '@/types';
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+    const router = useRouter();
+
+    const handleNavigation = () => {
+        // Calculate the new limit based on the page number and navigation type
+        const newLimit = (pageNumber + 1) * 10;
+
+        // Update the "limit" search parameter in the URL with the new value
+        const newPathname = updateSearchParams('limit', `${newLimit}`);
+
+        router.push(newPathname);
+    };
+
+    return (
+        <div className="w-full flex-center gap-5 mt-10">
+            {!isNext && (
+                <Button
+                    btnType="button"
+                    title="Show More"
+                    containerStyles="bg-primary-blue rounded-full text-white"
+                    handleClick={handleNavigation}
+                />
+            )}
+        </div>
+    );
+};
+
+export default ShowMore;
---
+++ src/app/Loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+    return <span className="w-10 h-10 rounded-full border-4 border-t-transparent border-blue-500 animate-spin"></span>;
+}
